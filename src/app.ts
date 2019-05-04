38c89052487d4b77653ef4ac5d3fc60818ded19e
Steve Song <sshtel@gmail.com>
2019-05-04 15:48:00 +0900

    added nuxt.js framework

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,54 @@
 import * as express from 'express';
 
-// import * as jwt from 'jsonwebtoken';
-// import * as request from 'superagent';
-// import * as querystring from 'querystring';
 import { Upbit } from 'upbit-js';
-// import { UpbitCron } from './cron/upbit-cron';
 import { RoutePublic } from './router/route-public';
 import { RouteUpbit } from './router/route-upbit';
+
+// Nuxt Framework
+import * as consola from 'consola';
+import { Builder, Nuxt } from 'nuxt';
+
+// Import and Set Nuxt.js options
+import * as config from '../nuxt.config.js';
+config.dev = !(process.env.NODE_ENV === 'production');
 
 const app = express();
 
 const upbit = new Upbit();
-// const upbitCron = new UpbitCron();
 
-// let candlesMinutes = {};
-// upbitCron.on('candlesMinutes', value => {
-//   candlesMinutes = value;
-// });
+async function start() {
+  // Init Upbit
+  console.log(upbit.getAccessToken());
+  console.log(upbit.getSecretToken());
 
-// let orderBook = {};
-// upbitCron.on('orderBook', value => {
-//   orderBook = value;
-// });
+  RouteUpbit.set(app);
+  RoutePublic.set(app);
 
-console.log(upbit.getAccessToken());
-console.log(upbit.getSecretToken());
-// const query = querystring.queryEncode({/* 요청할 파라미터 */});
+  const PORT = process.env.PORT || 8080;
 
-RouteUpbit.set(app);
-RoutePublic.set(app);
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config);
+  const { host, port } = nuxt.options.server;
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}!`);
-});
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt);
+    await builder.build();
+  } else {
+    await nuxt.ready();
+  }
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render);
+
+  // Listen the server
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}!`);
+  });
+
+  consola.default.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  });
+}
+start();
